5f8e142359e796ea24bf5e82f72c3bd70fda2d2c
DimanthaWalallawita <walallawitad@gmail.com>
2023-08-13 01:41:07 -0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Home from "./pages/home/Home";
 import React from "react";
-import Navbar from "./components/Navbar";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import Forgot from "./pages/auth/Forgot";
+import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
-    <React.Fragment>
-      <Navbar/>
-    </React.Fragment>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path="/login" element={<Login/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path="/register" element={<Register/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path="/forgot" element={<Forgot/>}/>
+      </Routes>
+
+      <Routes>
+        <Route path="/profile" element={<Profile/>}/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,52 +1,73 @@
-/*import React from 'react'
-import {BilogIn} from "react-icons/bi"
-import {FaUserCircle} from "react-icons/fa"
+import React from 'react';
+import "../style/main.css";
+import {Link,NavLink,useNavigate} from "react-router-dom";
 
-export const Header = () => {
+
+const activeLink = ({isActive}) => (isActive ? "active" : "");
+
+const Header = () => {
+
+    const navigate = useNavigate()
+
+    const goHome = () => {
+        navigate("/")
+    }
+
   return (
-    <header>
+
+    <section id="header">
+        <a href="#"><img src="" class="logo" alt=""/></a>
+
+        <div>
+            <ul id="navbar">
+                <li><a className="active" href="/">Home</a></li>
+                <li><a href="/register">Register</a></li>
+                <li><a href="/login">Login</a></li>
+            </ul>
+        </div>
+
+        <div id="mobile">
+            <a href="#"><i class="fa fa-shopping-bag"></i></a>
+            <i id="bar" class="fa fa-outdent"></i>
+        </div>
+    </section>
+
+    /*<header className="header">
         <nav>
-            <div className="logo">
-                <BilogIn size={35}/>
-                <span>LOGO</span>
+            <div className="logo" onClick={goHome}>
+                <BiLogIn size={35}/>
+                <span>JOB-Managment</span>
             </div>
-
-
             <ul className="home-links">
                 <li className="--flex-center">
                     <FaUserCircle size={20}/>
+
                     <p className="--color-white">
-                        Hi, Dimantha
+                        Hi Dimantha
                     </p>
+                </li>
+                <li>
+                    <button className="btn">
+                        <Link to="/login">Login</Link>
+                    </button>
+                </li>
+                
+                <li>
+                    
+                <NavLink to="/profile" className={activeLink}>Profile</NavLink>
+                    
+                </li>
+
+                <li>
+                    <button className="nav-close-btn">
+                        Logout
+                    </button>
                 </li>
             </ul>
         </nav>
     </header>
+  */
   )
 }
 
-export default Header*/
-
-import React from "react";
-import {FaBars, FaTimes} from "react-icons/fa"
-
-function Navbar(){
-    return(
-        <header>
-            <h3>LOGO</h3>
-            <nav>
-                <a href="#">Home</a>
-                <a href="#">Login</a>
-                <a href="#">Register</a>
-                <button>
-                    <FaTimes/>
-                </button>
-            </nav>
-            <button>
-                <FaBars/>
-            </button>
-        </header>
-    );
-}
-
-export default Navbar;
+export default Header
